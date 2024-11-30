7e2922953dea732e613c1479a6230351d596facf
maivan.quang <hogiut@gmail.com>
2024-12-01 00:16:03 +0900

    add something on about.component html

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener,ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css'],
+  encapsulation: ViewEncapsulation.None, // グローバルに適用
 })
 export class AboutComponent {
-
+  @HostListener('window:scroll', ['$event'])
+  onScroll(): void {
+    const animatedImages = document.querySelectorAll('.animated-image');
+    animatedImages.forEach((image) => {
+      const rect = image.getBoundingClientRect();
+      if (rect.top <= window.innerHeight * 0.75) {
+        image.classList.add('appear');
+      }
+    });
+  }
 }
